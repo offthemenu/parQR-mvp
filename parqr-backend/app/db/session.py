9068a56eb08cee699e20471b98d6cf72de348541
offthemenu <ywc261@nyu.edu>
2025-07-04 09:47:16 +0900

    Fix database URL scheme in session.py to use mysqldb

--- parqr-backend/app/db/session.py
+++ parqr-backend/app/db/session.py
@@ -11,7 +11,7 @@ DB_NAME=os.getenv("DB_NAME")
 DB_HOST=os.getenv("DB_HOST")
 DB_PORT=os.getenv("DB_PORT")
 
-DATABASE_URL=f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL=f"mysql+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
