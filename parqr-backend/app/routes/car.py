2dc2508d74fb08790b87773bf07fcfa81f6e2d35
offthemenu <ywc261@nyu.edu>
2025-07-15 15:09:48 +0900

    refactor: fixed according to version (unpacking dict -> **model_dump)

--- parqr-backend/app/routes/car.py
+++ parqr-backend/app/routes/car.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Session
 from app.schemas.car_schema import CarCreate, CarOut
 from app.db.base import get_db
 from app.models import car as car_model
+from pydantic import model_dump
 
 router = APIRouter(prefix="/v01/car")
 
 @router.post("/register", response_model=CarOut)
 def register_car(car: CarCreate, db: Session = Depends(get_db)):
-    new_car = car_model.Car(**car.dict())
+    new_car = car_model.Car(**model_dump(car))
     db.add(new_car)
     db.commit()
     db.refresh(new_car)
